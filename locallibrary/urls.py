eaee2f5257664a7f3b3bf101689c88980e9128ec
lanahacw <lanahacw@dukes.jmu.edu>
2025-04-10 08:58:40 -0400

    thing

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
 ]
 
 # Use include() to add paths from the catalog application
