746ff74d6f35a9e5f19d945d6193de601b41e078
lanahacw <lanahacw@dukes.jmu.edu>
2025-04-10 09:02:11 -0400

    thing

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
 
 # Use include() to add paths from the catalog application
